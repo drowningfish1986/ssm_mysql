59aad376a7d56a37f065a627e557d63de7d58fa4
unknown <yuliu109@creditease.cn>
2019-01-08 10:24:31 +0800

    commit

--- mysqlbox/src/main/java/com/fish/controller/CreditCalculatorController.java
+++ mysqlbox/src/main/java/com/fish/controller/CreditCalculatorController.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController("/fishbox")
 public class CreditCalculatorController {
 
     @PostMapping("/credit/calculate")
